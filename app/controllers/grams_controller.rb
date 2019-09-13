ce3b648b7ed9ed515b9ff8a2c0d9be64c2a37e66
mcv1993 <mcv1993@gmail.com>
2019-09-13 04:12:28 +0000

    L15

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -1,5 +1,23 @@
 class GramsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+
+  def index
+  end
+
+  def new
+    @gram = Gram.new
+  end
+
+  def create
+    @gram = current_user.grams.create(gram_params)
+
+    if @gram.valid?
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
   def show
     @gram = Gram.find_by_id(params[:id])
     return render_not_found if @gram.blank?
@@ -8,11 +26,13 @@ class GramsController < ApplicationController
   def edit
     @gram = Gram.find_by_id(params[:id])
     return render_not_found if @gram.blank?
+    return render_not_found(:forbidden) if @gram.user != current_user
   end
 
   def update
     @gram = Gram.find_by_id(params[:id])
     return render_not_found if @gram.blank?
+    return render_not_found(:forbidden) if @gram.user != current_user
 
     @gram.update_attributes(gram_params)
     if @gram.valid?
@@ -22,29 +42,14 @@ class GramsController < ApplicationController
     end
   end
 
-  def index
-  end
-
-  def new
-    @gram = Gram.new
-  end
-  
-  def create
-    @gram = current_user.grams.create(gram_params)
-    if @gram.valid?
-      redirect_to root_path
-    else
-      render :new, status: :unprocessable_entity
-    end
-  end
-
   def destroy
     @gram = Gram.find_by_id(params[:id])
     return render_not_found if @gram.blank?
+    return render_not_found(:forbidden) if @gram.user != current_user
     @gram.destroy
     redirect_to root_path
-
   end
+
 
   private
 
@@ -52,8 +57,9 @@ class GramsController < ApplicationController
     params.require(:gram).permit(:message)
   end
 
-  def render_not_found
-    render plain: 'Not Found :(', status: :not_found
+
+  def render_not_found(status=:not_found)
+    render plain: "#{status.to_s.titleize} :(", status: status
   end
 
 end
